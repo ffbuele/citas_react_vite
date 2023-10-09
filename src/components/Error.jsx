e66e47c035e9b997cee8cd8bd0ddf37c00e1f190
ffbuele <panchobuele98@gmail.com>
2023-10-09 18:38:35 -0500

    first and last commit

---
+++ src/components/Error.jsx
@@ -0,0 +1,6 @@
+export default function Error({children}) {
+    return (
+        <div className="bg-red-800 text-center text-white p-3 uppercase font-bold mb-3 rounded-lg">
+            {children}
+        </div>)
+}
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  // content: ["./src/**/*.{html,js,jsx}"],
+  content: ["./index.html", "./src/**/*.jsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
